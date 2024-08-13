4e30d6e79f95e397b748bae82b4f90fbc9e38f7e
Mohinur Ismoilova <mohinurismoilova@MacBook-Pro-Mohinur.local>
2024-08-13 22:57:03 +0500

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from './layout/PageHeader'
+
+const App = () => {
+  return (
+    <div className='max-h-screen flex flex-col'>
+      {/* <div>1</div>   
+      <div>2</div> */}
+     <PageHeader/>
+    </div>
+  )
+}
+
+export default App
